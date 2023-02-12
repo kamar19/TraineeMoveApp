e7d3a053782e9a5f1c5431b172dc4b9838a171a1
kamar19 <kaimar@mail.ru>
2023-02-12 11:38:25 +0600

    Зависимости + Заполнение списка films

--- app/src/main/java/com/example/traineemoveapp/MainActivity.kt
+++ app/src/main/java/com/example/traineemoveapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.traineemoveapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,10 +11,21 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+
 import com.example.traineemoveapp.ui.theme.TraineeMoveAppTheme
+import com.example.traineemoveapp.viewModel.MainActivityViewModel
+import kotlinx.coroutines.InternalCoroutinesApi
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
+
+
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    val viewModel: MainActivityViewModel by viewModels()
+
+    @OptIn(InternalCoroutinesApi::class) override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TraineeMoveAppTheme {
@@ -23,15 +35,24 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect {
+                    // Update UI elements
+                }
+            }
+        }
+
+
     }
-}
 
-@Composable fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable fun Greeting(name: String) {
+        Text(text = "Hello $name!")
+    }
 
-@Preview(showBackground = true) @Composable fun DefaultPreview() {
-    TraineeMoveAppTheme {
-        Greeting("Android")
+    @Preview(showBackground = true) @Composable fun DefaultPreview() {
+        TraineeMoveAppTheme {
+            Greeting("Android")
+        }
     }
 }
